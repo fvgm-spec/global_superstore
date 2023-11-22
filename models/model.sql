4f4645843af07b3aabd9980ef6907fda447d9e80
felix Gutierrez <felixvidalgu@gmail.com>
2023-11-22 14:55:52 -0300

    updating model

--- models/model.sql
+++ models/model.sql
@@ -3,3 +3,5 @@
 SELECT
     *
 FROM global_superstore_orders gso
+LEFT JOIN global_superstore_returns gsr ON gsr."Order ID" = gso."Order ID"
+LEFT JOIN global_superstore_people gsp ON gsp."Region" = gso."Region"
