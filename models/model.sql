a8711ca0018b810ab1e79edc2a397c23f25f8a46
felix Gutierrez <felixvidalgu@gmail.com>
2023-11-23 14:04:12 -0300

    updating dashboard, model

--- models/model.sql
+++ models/model.sql
@@ -1,7 +1,9 @@
 
 
 SELECT
-    *
+    gso.*,
+    gsr."Order ID",
+    gsr.Market
 FROM global_superstore_orders gso
 LEFT JOIN global_superstore_returns gsr ON gsr."Order ID" = gso."Order ID"
 LEFT JOIN global_superstore_people gsp ON gsp."Region" = gso."Region"
